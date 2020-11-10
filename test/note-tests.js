69c181f62ba464ae0ed59bec4d25503c5fd9a5d0
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-11-10 14:25:02 +0000

    First commit, wrote two tests for Note class both pass.

---
+++ test/note-tests.js
@@ -0,0 +1,14 @@
+// note-tests.js
+
+function testNotesExist() {
+  var note = new Note("My favourite language is JavaScript");
+  assert.isTrue(note.savednotes === "My favourite language is JavaScript");
+};
+
+function testNotesReturn() {
+  var note = new Note("My favourite language is JavaScript");
+  assert.isTrue(note.returnNote() === "My favourite language is JavaScript");
+};
+
+testNotesExist();
+testNotesReturn()
