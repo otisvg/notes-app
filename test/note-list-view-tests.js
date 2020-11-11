fd0dea23893d77fc27223f1e6f1b648d69da9b91
Otis <otisvickersgraver@gmail.com>
2020-11-11 17:16:19 +0000

    note list view returns each note in html list

--- test/note-list-view-tests.js
+++ test/note-list-view-tests.js
@@ -1,10 +1,12 @@
-(function testListViewReturns(exports) {
-  //mocking?
+(function testListViewReturns() {
+
   var noteList = new Notelist();
   noteList.addNote("Fav food = beef");
   noteList.addNote("Fav drink = beer");
 
-  var noteListView = new Notelistview(noteList.returnNotes());
+  var noteListView = new Notelistview(noteList.savedNotes);
+  console.log(noteListView.allNotes());
 
-  assert.isTrue(noteListView.allNotes() === "Fav food = beef, Fav drink = beer");
+  assert.isTrue(noteListView.allNotes() === `<ul><li><div>Fav food = beef</div></li><li><div>Fav drink = beer</div></li></ul>`
+    );
 })(this);
